0056844651ca3c438cd269bdf770ce7c93759743
ReysAden <Adenreys1@gmail.com>
2026-04-02 10:20:18 -0500

    flask app updated to display invenotry

--- flaskapp.py
+++ flaskapp.py
@@ -9,55 +9,22 @@ from dbCode import *
 
 app = Flask(__name__)
 app.secret_key = 'your_secret_key' # this is an artifact for using flash displays; 
-                                   # it is required, but you can leave this alone
+# it is required, but you can leave this alone
 
 @app.route('/')
 def home():
     return render_template('home.html')
-
-@app.route('/add-user', methods=['GET', 'POST'])
-def add_user():
-    if request.method == 'POST':
-        # Extract form data
-        f_name = request.form['f_name']
-        l_name = request.form['l_name']
-        genre = request.form['genre']
-        
-        # Process the data (e.g., add it to a database)
-        # For now, let's just print it to the console
-        print("Name:", f_name + " "+ l_name, ":", "Favorite Genre:", genre)
-        
-        flash('User added successfully! Huzzah!', 'success')  # 'success' is a category; makes a green banner at the top
-        # Redirect to home page or another page upon successful submission
-        return redirect(url_for('home'))
-    else:
-        # Render the form page if the request method is GET
-        return render_template('add_user.html')
-
-@app.route('/delete-user',methods=['GET', 'POST'])
-def delete_user():
-    if request.method == 'POST':
-        # Extract form data
-        name = request.form['name']
-        
-        # Process the data (e.g., add it to a database)
-        # For now, let's just print it to the console
-        print("Name to delete:", name)
-        
-        flash('User deleted successfully! Hoorah!', 'warning') 
-        # Redirect to home page or another page upon successful submission
-        return redirect(url_for('home'))
-    else:
-        # Render the form page if the request method is GET
-        return render_template('delete_user.html')
-
-
-@app.route('/display-users')
+# Displaying Items in the inventory
+@app.route('/display-inventory-items')
 def display_users():
-    # hard code a value to the users_list;
-    # note that this could have been a result from an SQL query :) 
-    users_list = (('John','Doe','Comedy'),('Jane', 'Doe','Drama'))
-    return render_template('display_users.html', users = users_list)
+    # load inventory from database
+    try:
+        inventory_list = execute_query('SELECT * FROM Inventory')
+    except Exception as e:
+        flash(f'Error loading inventory: {e}', 'danger')
+        inventory_list = []
+
+    return render_template('display_inventory.html', users=inventory_list)
 
 
 # these two lines of code should always be the last in the file
